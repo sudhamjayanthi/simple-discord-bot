3917d0a74e8ff73b020d24e5c038ebb7e21a6a61
Sudham Jayanthi <workwithsudham@gmail.com>
2022-09-12 23:27:04 +0530

    idk smthg backup push cp

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
 TOKEN= getenv("TOKEN")
 
-connect = discord.Client()
+bot = commands.Bot("bot ")
 
-@connect.event
+@bot.event
 async def on_ready():
     print('Bot initialized')
 
-@connect.event
-async def on_message(message):
-    if message.author == connect.user:
-        return 
-    if True: 
-        await message.channel.send(f"{message.author} just messaged!")
+@bot.command()
+async def test(ctx):
+    await ctx.send("This is just test command \n ~ MidNightBot 2020")
 
-connect.run(TOKEN)
+@bot.command(help='This command says your message. \nFormat: "bot say <message>"')
+async def say(ctx,arg1 = None):
+    if not arg1: await ctx.send("No message adeded ! See bot help say")
+    else: await ctx.send(f"{ctx.author} wants me to say ```{arg1}```")
+
+
+bot.run(TOKEN)
